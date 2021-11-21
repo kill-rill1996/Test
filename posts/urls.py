4401a263ac7229f75206ca4b4ea89467247c10b2
Kirill Andreev <hizenberg228@mail.ru>
2021-11-21 23:29:05 +0300

    CRUD Comments with 1 bug

--- posts/urls.py
+++ posts/urls.py
@@ -1,17 +1,19 @@
 from django.urls import path
 from rest_framework.routers import SimpleRouter
 
-from .views import PostViewSet, CommentView
+from .views import PostViewSet, CommentView, CommentMixinsView
 
 
 urlpatterns = [
+    path('comments/<int:pk>/', CommentMixinsView.as_view()),
+    path('comments/create/', CommentView.as_view()),
+
+    path('posts/<int:pk>/comments/', CommentView.as_view()),
     path('posts/', PostViewSet.as_view({'get': 'list'})),
     path('posts/create/', PostViewSet.as_view({'post': 'create'})),
     path('posts/<int:pk>/', PostViewSet.as_view({'get': 'retrieve'})),
-    path('posts/<int:pk>/update', PostViewSet.as_view({'put': 'update'})),
-    path('posts/<int:pk>/delete', PostViewSet.as_view({'delete': 'destroy'})),
-
-    path('posts/comments/<int:pk>/', CommentView.as_view()),
+    path('posts/update/<int:pk>/', PostViewSet.as_view({'put': 'update'})),
+    path('posts/delete/<int:pk>/', PostViewSet.as_view({'delete': 'destroy'})),
 
 ]
 
--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,8 @@
 from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.decorators import api_view, permission_classes
+from rest_framework.generics import GenericAPIView
+from rest_framework.mixins import CreateModelMixin, DestroyModelMixin, RetrieveModelMixin, UpdateModelMixin
 from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from rest_framework import status
 
@@ -23,6 +24,7 @@ from rest_framework.viewsets import ModelViewSet
 class PostViewSet(ModelViewSet):
     model = Post
     serializer_class = PostModelSerializer
+    # permission_classes = []
 
     def get_permissions(self):
         if self.action == 'create':
@@ -45,10 +47,13 @@ class CommentView(APIView):
         comments_tree = create_comments_tree(post.comments.all())
         return Response(comments_tree)
 
-    def post(self, request, pk):
+    def post(self, request):
         try:
             data = request.data
-            data['post'] = pk
+            if data['parent']:
+                data['is_child'] = True
+                # if Comment.objects.get(id=data['parent']).post != data['post']:
+                #     return Response(status=status.HTTP_400_BAD_REQUEST)
             comment = CommentSerializer(data=request.data)
             if comment.is_valid():
                 comment.save()
@@ -58,3 +63,16 @@ class CommentView(APIView):
         except AssertionError:
             return Response(status=status.HTTP_400_BAD_REQUEST)
 
+
+class CommentMixinsView(GenericAPIView, DestroyModelMixin, UpdateModelMixin, RetrieveModelMixin):
+    serializer_class = CommentSerializer
+    queryset = Comment.objects.all()
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
